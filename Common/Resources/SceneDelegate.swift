58130661352bbf91e7d5f0ec9f7826ec266f8c24
Timur - Kuvvat <b.l.a.c.k.w.o.l.f@mail.ru>
2025-12-04 17:22:09 +0100

    "Move Resources dir to Common/Resources"

--- Common/Resources/SceneDelegate.swift
+++ Common/Resources/SceneDelegate.swift
@@ -11,15 +11,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    var appCoordinator: AppCoordinator?
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TabBarController()
-//        window?.rootViewController = AddRecordViewController()
-        window?.makeKeyAndVisible()
-        
+
+        windowScene.traitOverrides.appTheme = ThemeManager.shared.userTheme
+//        print("windowScene: \(windowScene.traitOverrides.appTheme.name)")
+
+            // Создаем зависимости
+        let viewModelFactory = ViewModelFactory(dependencies: .shared)
+        let viewFactory = ViewControllersFactory(viewModelsFactory: viewModelFactory)
+
+            // Создаем и запускаем главный координатор
+        let coordinator = AppCoordinator(viewFactory: viewFactory, window: window!)
+        self.appCoordinator = coordinator
+
+        coordinator.start()
+//        window?.rootViewController = TabBarController()
+//        window?.makeKeyAndVisible()
+
+
+
 //        print("🚀 Приложение запускается - проверяем необходимость CSV архивации")
 //        AppCoreServices.shared.archiveManager.checkArchiveOnAppStart()
         
